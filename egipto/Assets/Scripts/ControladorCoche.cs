ecf487690880a2d2b0b9f2ec9e885b0d5d7ad290
Macarena Rufo Palomo <mrufpal@gmail.com>
2017-05-16 09:42:57 +0200

    Creacion escena Help, giro con acelerometro, correccion errores

--- egipto/Assets/Scripts/ControladorCoche.cs
+++ egipto/Assets/Scripts/ControladorCoche.cs
@@ -28,10 +28,10 @@ public class ControladorCoche : MonoBehaviour
         CharacterController controller = GetComponent<CharacterController>();
 
             
-            moveDirection = new Vector3(0, 0, Input.GetAxis("Horizontal") * speedTurn);
+            moveDirection = new Vector3(0, 0, Input.acceleration.x * speedTurn);
             moveDirection = transform.TransformDirection(moveDirection);
             moveDirection *= speed;
-            giroEnY = Input.GetAxis("Horizontal") * anguloDeGiro;
+            giroEnY = Input.acceleration.x * anguloDeGiro;
 
             if(cocheGO.name=="taxi" )
                 cocheGO.transform.rotation = Quaternion.Euler(-90, giroEnY -90  ,0 );
